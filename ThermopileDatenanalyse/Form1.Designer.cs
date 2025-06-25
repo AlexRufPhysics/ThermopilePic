59b8fa6691bda7da6a4d0cd59f9dca91f569fe66
aleruf <aleruf@PRODI025>
2025-06-25 16:33:15 +0200

    Projektdateien hinzufügen.

---
+++ ThermopileDatenanalyse/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ThermopileDatenanalyse
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            plotView1 = new OxyPlot.WindowsForms.PlotView();
+            plotView2 = new OxyPlot.WindowsForms.PlotView();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // plotView1
+            // 
+            plotView1.Location = new Point(0, 0);
+            plotView1.Name = "plotView1";
+            plotView1.PanCursor = Cursors.Hand;
+            plotView1.Size = new Size(75, 23);
+            plotView1.TabIndex = 0;
+            plotView1.Text = "plotView1";
+            plotView1.ZoomHorizontalCursor = Cursors.SizeWE;
+            plotView1.ZoomRectangleCursor = Cursors.SizeNWSE;
+            plotView1.ZoomVerticalCursor = Cursors.SizeNS;
+            // 
+            // plotView2
+            // 
+            plotView2.BackColor = SystemColors.ControlLight;
+            plotView2.Location = new Point(56, 43);
+            plotView2.Name = "plotView2";
+            plotView2.PanCursor = Cursors.Hand;
+            plotView2.Size = new Size(379, 334);
+            plotView2.TabIndex = 1;
+            plotView2.Text = "plotView2";
+            plotView2.ZoomHorizontalCursor = Cursors.SizeWE;
+            plotView2.ZoomRectangleCursor = Cursors.SizeNWSE;
+            plotView2.ZoomVerticalCursor = Cursors.SizeNS;
+            plotView2.Click += plotView2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(582, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(plotView2);
+            Controls.Add(plotView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private OxyPlot.WindowsForms.PlotView plotView2;
+        private Button button1;
+    }
+}
---
+++ ThermopileDatenanalyse/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using OxyPlot.Axes;
+using System.Timers;
+
+
+
+namespace ThermopileDatenanalyse
+{
+    public partial class Form1 : Form
+    {
+        private string port = "COM7";
+        private System.Windows.Forms.Timer updateTimer = new System.Windows.Forms.Timer();
+        private int baudRate = 115200;
+
+        const int RowPerBlock = 7;
+        const int PixelPerRow = 120;
+        const int NumberOfBlocks = 6;
+        const int DataPos = 2;
+        const int PixelPerColumn = RowPerBlock * 2 * NumberOfBlocks;
+        const int DataBlockLengthPerBlock = 2 * PixelPerRow * RowPerBlock + 10;
+        const int TotalBlocks = 2 * NumberOfBlocks + 3;
+
+
+        private SerialPort serialPort = null!;
+        private PlotModel heatmapModel = null!;
+        private HeatMapSeries heatmapSeries = null!;
+
+        private byte[][] RAMoutput = new byte[TotalBlocks][];
+        private ushort[,] pixelData = new ushort[PixelPerColumn, PixelPerRow];
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitSerialPort();
+            InitPlot();
+        }
+
+        private void plotView2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        private void InitSerialPort()
+        {
+            serialPort = new SerialPort(port, baudRate);
+            serialPort.ReadTimeout = 1000;
+            serialPort.WriteTimeout = 500;
+        }
+
+        private void InitPlot()
+        {
+            heatmapModel = new PlotModel { Title = "Live Heatmap" };
+
+            heatmapSeries = new HeatMapSeries
+            {
+                X0 = 0,
+                X1 = PixelPerRow,
+                Y0 = 0,
+                Y1 = PixelPerColumn,
+                Interpolate = false,
+                RenderMethod = HeatMapRenderMethod.Bitmap,
+                Data = new double[PixelPerColumn, PixelPerRow]
+            };
+
+            heatmapModel.Series.Add(heatmapSeries);
+            heatmapModel.Axes.Add(new LinearColorAxis { Position = AxisPosition.Right });
+
+            plotView1.Model = heatmapModel;
+        }
+
+        private void InitTimer()
+        {
+            updateTimer.Interval = 100; // 100ms → ca. 10Hz
+            updateTimer.Tick += UpdateTimer_Tick;
+        }
+
+        private void UpdateTimer_Tick(object? sender, EventArgs e)
+        {
+            try
+            {
+                serialPort.Write("b");
+
+                // Alle Datenblöcke lesen
+                for (int i = 0; i < TotalBlocks; i++)
+                {
+                    RAMoutput[i] = new byte[DataBlockLengthPerBlock];
+                    int bytesRead = 0;
+                    while (bytesRead < DataBlockLengthPerBlock)
+                    {
+                        bytesRead += serialPort.Read(RAMoutput[i], bytesRead, DataBlockLengthPerBlock - bytesRead);
+                    }
+                }
+
+                ConvertToPixelData();
+                UpdateHeatmap();
+            }
+            catch (Exception ex)
+            {
+                updateTimer.Stop();
+                MessageBox.Show("Fehler beim Lesen/Zeichnen: " + ex.Message);
+            }
+
+
+        }
+
+        private void ConvertToPixelData()
+        {
+            ushort pos;
+            for (int m = 0; m < RowPerBlock; m++)
+            {
+                for (int n = 0; n < PixelPerRow; n++)
+                {
+                    pos = (ushort)(2 * n + DataPos + m * 2 * PixelPerRow);
+
+                    for (int i = 0; i < NumberOfBlocks; i++)
+                    {
+                        // top half
+                        pixelData[m + i * RowPerBlock, n] =
+                            (ushort)(RAMoutput[i][pos] << 8 | RAMoutput[i][pos + 1]);
+
+                        // bottom half
+                        pixelData[PixelPerColumn - 1 - m - i * RowPerBlock, n] =
+                            (ushort)(RAMoutput[2 * NumberOfBlocks + 2 - i - 1][pos] << 8 | RAMoutput[2 * NumberOfBlocks + 2 - i - 1][pos + 1]);
+                    }
+                }
+            }
+
+        }
+
+        private void UpdateHeatmap()
+        {
+            for (int y = 0; y < PixelPerColumn; y++)
+            {
+                for (int x = 0; x < PixelPerRow; x++)
+                {
+                    heatmapSeries.Data[y, x] = pixelData[y, x];
+                }
+            }
+
+            plotView1.InvalidatePlot(true);
+        }
+    }
+
+
+}
